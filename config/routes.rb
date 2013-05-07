abe42f4e0397f67e2dc7e634813d26bd308f33b1
Cyrus-Baumgarten <cdbaumg@gmail.com>
2013-05-07 14:24:54 -0700

    Finish demo app

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 DemoApp::Application.routes.draw do
+  resources :microposts
+  resources :users
+end
+
+DemoApp::Application.routes.draw do
+  resources :microposts
+  resources :users
+end
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -55,4 +63,4 @@ DemoApp::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
-end
+
